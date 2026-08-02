94fee9aac6f8590d69eaca67fdd7fe31b6f89a98
Megdelawit365 <megdelawitaynalem365@gmail.com>
2026-08-02 14:04:20 +0300

    added updatebooks function

--- services/bookService.js
+++ services/bookService.js
@@ -31,4 +31,73 @@ const viewAllBooks = () => {
     }
 }
 
-export { addBook, viewAllBooks }
+const searchBooks = (query) => {
+    // query is an object
+
+    let field = ""
+    let value = ""
+    const result = []
+
+    for (const key in query) {
+        field = key
+        value = query[key]
+    }
+
+    for (const b of books) {
+        if (b[field] === value) {
+            result.push(b)
+        }
+    }
+
+    if (result.length == 0) {
+        console.log("No books found.")
+        return
+    }
+
+    console.log("============= Search Result =============")
+    for (const r of result) {
+        console.log(r)
+    }
+}
+
+const updateBook = (book) => {
+    // check if book exists and isbn is valid
+    let existingBook = -1
+
+    for (let i = 0; i < books.length; i++) {
+        if (books[i].id == book.id) {
+            existingBook = i
+
+            // if field is isbn
+            if (book["isbn"] != books[i].isbn) {
+                console.log("Isbn cannot be changed.")
+                return
+            }
+        }
+        if (books[i].isbn == book.isbn && books[i].id != book.id) {
+            console.log("Isbn already exists.")
+            return
+        }
+    }
+
+    if (existingBook == -1) {
+        console.log("Book does not exist.")
+        return
+    }
+
+    const isValid = validateBook(book, books)
+
+    if (!isValid.valid) {
+        console.log("============= Error =============")
+        console.log(isValid.error)
+        return
+    }
+
+
+    books[existingBook] = book
+    console.log("Book editted successfully.")
+    return
+
+}
+
+export { addBook, viewAllBooks, searchBooks }
