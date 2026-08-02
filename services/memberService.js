c8ffd00bb0d769f257988e4e904c95f69c8103c6
Megdelawit365 <megdelawitaynalem365@gmail.com>
2026-08-02 14:48:18 +0300

    added viewAll,  search and update members functions

--- services/memberService.js
+++ services/memberService.js
@@ -20,4 +20,81 @@ const addMember = (member) => {
     return
 }
 
+const viewAllMembers = () => {
+    if (members.length == 0) {
+        console.log("No members found.")
+        return
+    }
+    console.log(`${members.length} members found:`)
+    console.log(" ")
 
+    for (const m of members) {
+        console.log(m)
+    }
+}
+
+
+const searchMembers = (query) => {
+    // query is an object
+
+    let field = ""
+    let value = ""
+    const result = []
+
+    for (const key in query) {
+        field = key
+        value = query[key]
+    }
+
+    for (const m of members) {
+        if (m[field] === value) {
+            result.push(m)
+        }
+    }
+
+    if (result.length == 0) {
+        console.log("No members found.")
+        return
+    }
+
+    console.log("============= Search Result =============")
+    for (const r of result) {
+        console.log(r)
+    }
+}
+
+
+const updateMember = (member) => {
+    // check if member exists 
+    let existingmember = -1
+    for (let i = 0; i < members.length; i++) {
+        if (members[i].id == member.id) {
+            existingmember = i
+        }
+    }
+
+    if (existingmember == -1) {
+        console.log("member does not exist.")
+        return
+    }
+
+
+    const isValid = validateMember(member, members)
+
+    if (!isValid.valid) {
+        console.log("============= Error =============")
+        console.log(isValid.error)
+        return
+    }
+
+
+    members[existingmember] = member
+
+    console.log("member editted successfully.")
+    console.log("Editted member:")
+    console.log(members[existingmember])
+    return
+
+}
+
+export { addMember, viewAllMembers, searchMembers, updateMember }
