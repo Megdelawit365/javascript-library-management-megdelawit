7d2cbde6dfb904f8a941d2d4c22be3185d964bde
Megdelawit365 <megdelawitaynalem365@gmail.com>
2026-08-02 14:34:47 +0300

    editted generateId function to work for both books and members

--- utils/helpers.js
+++ utils/helpers.js
@@ -3,18 +3,18 @@ const isEmpty = (data) => {
     return data == null || data == ""
 }
 
-const generateId = (books) => {
-    const len = books.length
+const generateId = (data) => {
+    const len = data.length
     if (len == 0) {
         return 1
     }
     let id = 1
     // sort by id from lowest to highest
-    books.sort((a, b) => a.id - b.id)
+    data.sort((a, b) => a.id - b.id)
 
     // after sorting by id, generated id would be the smallest available number
     for (let i = 1; i <= len; i++) {
-        if (i != books[i - 1].id) {
+        if (i != data[i - 1].id) {
             id = i
             break
         }
